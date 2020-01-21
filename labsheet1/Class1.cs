17f8f3eed7ec53bd9c9909282da623117a5ed9e2
S00188044 <S00188044@LAB-B1042-29>
2020-01-21 12:36:18 +0000

    Added class

---
+++ labsheet1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labsheet1
+{
+    class Band
+    {
+        public string Bandname { get; set; }
+        public DateTime YearFormed { get; set; }
+        public string Members { get; set; }
+
+    }
+}
